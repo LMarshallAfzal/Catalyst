933c34ce031ba2e028489053a37c781c32965f31
Leonard Marshall Afzal <loma256@gmail.com>
2024-02-10 17:49:02 +0000

    Wrap configuration reading in a try/catch block

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 using System.Net;
-using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Accessing Configuration
 var config = builder.Configuration;
-int port = config.GetValue<int>("ServerSettings:Port");
-string hostAddress = config.GetValue<string>("ServerSettings:HostAddress") ?? "localhost";
+int port;
+string hostAddress;
+
+try 
+{
+    port = config.GetValue<int>("ServerSettings:Port");
+    hostAddress = config.GetValue<string>("ServerSettings:HostAddress") ?? "localhost";
+}
+catch (Exception e)
+{
+    Console.Error.WriteLine("Error reading configuration: " + e.Message);
+
+    port = 8080;
+    hostAddress = "127.0.0.1";
+}
 
 // Configure Kestrel
 builder.WebHost.ConfigureKestrel(options => {
